0e0e4fab6fc825fa54d820fd0114293f9254b675
BrnDnzDzgn <bdduzgun@outlook.com>
2025-03-23 18:14:49 +0300

    home page updated, moving personality cards added

---
+++ fantasy-realm-ui/src/components/ui/card.jsx
@@ -0,0 +1,56 @@
+"use client";
+import React from "react";
+import { Icon } from "./icon";
+import { AnimatePresence, motion } from "motion/react";
+
+const Card = ({
+  title,
+  icon,
+  children,
+  className
+}) => {
+  const [hovered, setHovered] = React.useState(false);
+
+  return (
+    <div
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      className={`border border-black/[0.2] group/canvas-card flex items-center justify-center
+        ${className}
+        dark:border-white/[0.2]
+        min-w-[20rem] p-20 relative h-96 sm:h-[30rem] md:h-[35rem] lg:h-[45rem]`}
+    >
+      {/* Corner Icons */}
+      <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
+      <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
+      <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
+      <Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
+
+      {/* Hover Overlay */}
+      <AnimatePresence>
+        {hovered && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="h-full w-full absolute inset-0"
+          >
+            {children}
+          </motion.div>
+        )}
+      </AnimatePresence>
+
+      {/* Static Content */}
+      <div className="relative z-20">
+        <div className="text-center group-hover/canvas-card:-translate-y-4 group-hover/canvas-card:opacity-0 transition duration-200 w-full mx-auto flex items-center justify-center">
+          {icon}
+        </div>
+        <h2 className="dark:text-white text-xl opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4 font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200">
+          {title}
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export { Card };
--- fantasy-realm-ui/src/pages/home/Home.jsx
+++ fantasy-realm-ui/src/pages/home/Home.jsx
@@ -1,9 +1,34 @@
-function Home() {
-    return (
-        <div className="p-10 bg-white shadow-md rounded-lg max-w-[800px] w-full">
-            <h1>Home</h1>
-        </div>
-    );
-}
+"use client";
+import React from "react";
+import { CanvasRevealEffect } from "../../components/ui/canvas-reveal-effect";
+import {Card} from "../../components/ui/card"
+import { AceternityIcon } from "../../components/ui/aceternityIcon";
 
-export default Home;
+export function Home() {
+const cardData = [
+  { title: "VMHFT", icon: <AceternityIcon /> },
+  { title: "VMHFR", icon: <AceternityIcon /> },
+  { title: "VMHGT", icon: <AceternityIcon /> },
+  { title: "VMHGR", icon: <AceternityIcon /> },
+  { title: "VMXFT", icon: <AceternityIcon /> },
+  { title: "VMXFR", icon: <AceternityIcon /> },
+  { title: "VMXGT", icon: <AceternityIcon /> },
+  { title: "VMXGR", icon: <AceternityIcon /> },
+  { title: "VWHFT", icon: <AceternityIcon /> },
+
+];
+
+return (
+    <>
+        <div className="relative w-full overflow-hidden bg-white dark:bg-black py-20 bg-black">
+            <div className="flex gap-4 w-max" style={{ animation: 'slide 20s linear infinite' }}>
+                {[...cardData].map((card, index) => (
+                  <Card key={index} title={card.title} icon={card.icon}>
+                    <CanvasRevealEffect animationSpeed={5.1} containerClassName="bg-emerald-900" />
+                  </Card>
+                ))}
+            </div>
+        </div>
+    </>
+);
+}
